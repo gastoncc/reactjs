2771f9e8074ad5c7ea0b96b528b9abfe027c7b2b
gastoncc <gastonasura99@gmail.com>
2023-10-07 23:24:43 -0300

    primera entrega

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+        return (
+            <nav>
+                <h1>Bienvendos</h1>
+                <div>
+                    <button>Comics Marvel</button>
+                    <button>Comics DC</button>
+                    <button>Manga</button>
+                </div>
+            </nav>
+        )
+    }
+export default Navbar 
